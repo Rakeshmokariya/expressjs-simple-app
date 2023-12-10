73576be426056ecf0cd957c93fb2a74318d1b9d0
rakeshmokariya <rakeshmokariya.931@gmail.com>
2023-12-10 23:15:35 +0530

    init commit

---
+++ requires/queries.js
@@ -0,0 +1,86 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './.env' });
+
+const Pool = require("pg").Pool;
+
+const env = process.env;
+const pool = new Pool({
+  host: env.DB_HOST,
+  user: env.DB_USER,
+  password: env.DB_PASSWORD,
+  database: env.DB_NAME,
+  port: env.DB_PORT,
+});
+
+const getUsers = (request, response) => {
+  pool.query("SELECT * FROM users ORDER BY id ASC", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+const getUserById = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query("SELECT * FROM users WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+const createUser = (request, response) => {
+  const { name, email } = request.body;
+
+  pool.query(
+    "INSERT INTO users (name, email) VALUES ($1, $2)",
+    [name, email],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(201).send(`User added`);
+    }
+  );
+};
+
+const updateUser = (request, response) => {
+  const id = parseInt(request.params.id);
+  console.log(request.body)
+  const { name, email } = request.body;
+  console.log(name,email)
+
+  pool.query(
+    "UPDATE users SET name = $1, email = $2 WHERE id = $3",
+    [name, email, id],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).send(`User modified with ID: ${id}`);
+    }
+  );
+};
+
+const deleteUser = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query("DELETE FROM users WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).send(`User deleted with ID: ${id}`);
+  });
+};
+
+
+module.exports = {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+};
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const db = require("./requires/queries");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+app.get("/", (req, res) => {
+  res.json({ message: "ok" });
+});
+
+app.get("/users", db.getUsers);
+app.get("/users/:id", db.getUserById);
+app.post("/users", db.createUser);
+app.put("/users/:id", db.updateUser);
+
+
+app.listen(port, "0.0.0.0", () => {
+  console.log("runnign on port 3000");
+});
